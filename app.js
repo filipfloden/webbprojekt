3fe0bfb955178390e509d883f88aaff93389fdc7
Filip Flodén <33715227+filipfloden@users.noreply.github.com>
2020-10-11 16:43:52 +0200

    CRUD - portfolio

--- app.js
+++ app.js
@@ -1,24 +1,33 @@
 const express = require('express')
 const expressHandelbars = require('express-handlebars')
 const sqlite3 = require('sqlite3')
-var multer  = require('multer')
-var upload = multer({ dest: 'static/img/' })
+const multer  = require('multer')
+const path = require('path')
+//var upload = multer({ dest: 'static/img/' })
 const bodyParser = require('body-parser')
+
+const storage = multer.diskStorage({
+    destination: './img/portfolio/',
+    filename: function(req, file, cb){
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+    }
+})
+
+const upload = multer({
+    storage: storage
+}).single('image')
 
 const db = new sqlite3.Database("my-database.db")
 
-/*
+
 db.run(`
-    CREATE TABLE portfolio(
+    CREATE TABLE IF NOT EXISTS portfolio(
         id INTEGER PRIMARY KEY AUTOINCREMENT,
         title TEXT,
         description TEXT,
         image TEXT
     )
 `)
-*/
-
-
 
 
 const app = express()
@@ -38,11 +47,11 @@ app.use(express.static('static'))
 app.get('/main.css')
 app.get('/bootstrap.css')
 
-app.get('/', function(request, response){
-    response.render('start.hbs')
+app.get('/', function(req, res){
+    res.render('start.hbs')
 })
 
-app.get('/portfolioo', function(request, response){
+app.get('/portfolioo', function(req, res){
 
     const query = ("SELECT * FROM portfolio")
 
@@ -58,44 +67,96 @@ app.get('/portfolioo', function(request, response){
                 dbError: false,
                 projects
             }
-            console.log(model)
-            response.render('portfolio.hbs', model)
+            //console.log(model)
+            res.render('portfolio.hbs', model)
         }
     })
 })
-app.get('/create-project', function(request, response){
-    response.render('create-project.hbs')
+
+app.post('/portfolioo', function(req, res){
+
+    const title = req.body.title    
+    const description = req.body.description
+    const id = req.body.id
+    var query;
+    var values;
+    
+    if (req.body.btnID == "save") {
+        query = ("UPDATE portfolio SET title = ?, description = ? WHERE id = ?") 
+        values = [title, description, id]
+    }
+    else if (req.body.btnID == "delete"){
+        query = ("DELETE FROM portfolio WHERE id=?")
+        values = [id]
+    }
+    if (query != null) {
+        db.run(query, values, function(error){
+            if (error) {
+                console.log(error)
+            }
+            else{
+                res.redirect('/portfolioo')
+            }
+        })  
+    }else{
+        res.redirect('/portfolioo')
+    }
 })
 
-app.post('/create-project', function(request, response){
-    const title = request.body.title
-    const description = request.body.description
-    const image = request.body.image
-    const query = ("INSERT INTO portfolio (title, description, image) VALUES (?, ?, ?)")
-    const values = [title, description, image]
-
-    console.log(title, description, image)
+app.post('/delete', function(req, res){
+    const id = req.body.id
+    const query = ("DELETE FROM portfolio WHERE id=?")
+    const values = [id]
 
     db.run(query, values, function(error){
         if (error) {
             console.log(error)
         }
         else{
-            response.redirect('/portfolioo')
+            res.redirect('/portfolioo')
         }
     })
 })
 
-app.get('/about', function(request, response){
-    response.render('about.hbs')
+app.get('/create-project', function(req, res){
+    res.render('create-project.hbs')
 })
 
-app.get('/contact', function(request, response){
-    response.render('contact.hbs')
+app.post('/create-project', function(req, res){
+
+    upload(req, res, (error) =>{
+        if (error) {
+            console.log(error)
+        }
+        else{
+            const title = req.body.title    
+            const description = req.body.description
+            const image = req.file.filename
+            const query = ("INSERT INTO portfolio (title, description, image) VALUES (?, ?, ?)")
+            const values = [title, description, image]
+
+            db.run(query, values, function(error){
+                if (error) {
+                    console.log(error)
+                }
+                else{
+                    res.redirect('/portfolioo')
+                }
+            })
+        }
+    })
 })
 
-app.get('/faq', function(request, response){
-    response.render('faq.hbs')
+app.get('/about', function(req, res){
+    res.render('about.hbs')
+})
+
+app.get('/contact', function(req, res){
+    res.render('contact.hbs')
+})
+
+app.get('/faq', function(req, res){
+    res.render('faq.hbs')
 })
 
 app.listen(8080)
